93d240690a86348958dee4a8e2bb77388a1d2ed9
Obsidian <oe149499@hrsfc.ac.uk>
2023-11-11 16:58:26 +0000

    experiment with clap for argument parsing

--- project/server/src/main.rs
+++ project/server/src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+use camino::Utf8PathBuf;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short = 'r', long, default_value = ".")]
+    board_root: Utf8PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    dbg!(args);
 }
