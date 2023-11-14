f612505b94b8736dd1c11d85f29fb5de4e4a0c58
Obsidian <oe149499@hrsfc.ac.uk>
2023-11-14 14:47:00 +0000

    Set up more of basic web configuration

--- project/server/src/lib.rs
+++ project/server/src/lib.rs
@@ -11,3 +11,13 @@ pub fn create_api_filter() -> BoxedFilter<(impl Reply,)> {
 pub fn create_static_filter(path: PathBuf) -> BoxedFilter<(impl Reply,)> {
 	warp::fs::dir(path).boxed()
 }
+
+pub fn create_script_filter(path: PathBuf, enable_source: bool) -> BoxedFilter<(impl Reply,)> {
+	let main_filter = warp::fs::dir(path.join("out"));
+	if enable_source {
+		warp::path("source").and(warp::fs::dir(path))
+			.or(main_filter).unify().boxed()
+	} else {
+		main_filter.boxed()
+	}
+}
--- project/server/src/main.rs
+++ project/server/src/main.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 use clap::Parser;
 use camino::Utf8PathBuf;
 use tokio::runtime;
-use virtual_whiteboard::{create_api_filter, create_static_filter};
+use virtual_whiteboard::{create_api_filter, create_static_filter, create_script_filter};
 use warp::{Filter, filters::BoxedFilter, reply::Reply};
 
 #[derive(Parser, Debug)]
@@ -11,14 +11,22 @@ struct Args {
     #[arg(short = 'r', long, default_value = ".")]
     board_root: Utf8PathBuf,
 
-    #[arg(short, long = "static")]
+    #[arg(short = 's', long = "static-root")]
     static_path: Utf8PathBuf,
+
+    #[arg(short = 'j', long = "script-root")]
+    script_root: Utf8PathBuf,
+
+    #[arg(long, default_value_t = true)]
+    serve_ts: bool,
 }
 
 fn main() {
     let args = Args::parse();
     let filter = create_filter(
         args.static_path.into(),
+        args.script_root.into(),
+        args.serve_ts,
     );
     let runtime = runtime::Builder::new_multi_thread()
         .enable_io()
@@ -31,8 +39,10 @@ fn main() {
     })
 }
 
-fn create_filter(static_path: PathBuf) -> BoxedFilter<(impl Reply,)> {
+fn create_filter(static_path: PathBuf, script_path: PathBuf, enable_source: bool) -> BoxedFilter<(impl Reply,)> {
+    let index_filter = warp::path("index.html").and(warp::fs::file(static_path.join("index.html")));
     let api_filter = warp::path("api").and(create_api_filter());
     let static_filter = warp::path("static").and(create_static_filter(static_path));
-    return api_filter.or(static_filter).boxed()
+    let script_filter = warp::path("script").and(create_script_filter(script_path, enable_source));
+    return api_filter.or(static_filter).or(index_filter).or(script_filter).boxed()
 }
