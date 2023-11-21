a156192e54d40f93700b51a1a8e5c4a516bfadaf
Obsidian <oe149499@hrsfc.ac.uk>
2023-11-21 13:37:56 +0000

    Changes to test file

--- project/client/typescript/src/test.ts
+++ project/client/typescript/src/test.ts
@@ -1,8 +1,21 @@
+import { Item, Point } from "./gen/generated_types";
 /* eslint-disable @typescript-eslint/no-unused-vars */
 // Test file
 
-const test_type: TestType = {};
-const enum_test: EnumTest = { Message: {}, Type: {} };
+function point(x: number, y: number): Point {
+	return { x: x, y: y };
+}
+
+const basic_item: Item = {
+	"type": "Line",
+	"start": point(0, 0),
+	"end": point(0, 0),
+	"stroke": {
+		width: 10,
+		color: "red",
+	}
+};
 
 const message: string = "Hello World!";
-console.log(message);
+const item = JSON.stringify(basic_item);
+console.log(message, item);
